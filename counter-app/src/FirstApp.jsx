d5b5f5349f6fd15885f2b7dfaef3c8bd9a836b39
Bertha Janeth Pérez Canales <janeth_2124@hotmail.com>
2023-06-01 22:10:59 -0600

    Update and indicate that I left off at Class 37, Section 4

---
+++ counter-app/src/FirstApp.jsx
@@ -0,0 +1,10 @@
+
+export const FirstApp = () => {
+    return (
+        <>
+            <h1>Bertha Janeth Perez </h1>  
+            <p> Soy un subtitulo </p>
+        </>
+      
+    )
+  }
--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDom from 'react-dom/client';
+//import {App,  HelloApp}  from './App';
+import {FirstApp} from './FirstApp'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+ReactDom.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <FirstApp/>
+    </React.StrictMode>
 )
